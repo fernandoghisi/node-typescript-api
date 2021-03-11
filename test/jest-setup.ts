014872d475d384e972d73a5858bdc84ad3d6bd85
Fernando Ghisi <fernandoghisi@gmail.com>
2021-03-11 12:49:07 -0300

    initial server config (using express and overnightjs) and functional tests configuration

---
+++ test/jest-setup.ts
@@ -0,0 +1,14 @@
+import supertest from 'supertest';
+
+import { SetupServer } from '@src/server';
+
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
+  //await server.init();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
+
+//afterAll(async () => await server.close());
